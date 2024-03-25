7d1b81459418a792b76ecfe5bb99f7e9ea6d43e0
Vanderson Alves <vanalvesdev@gmail.com>
2024-03-25 17:48:56 -0300

    refactor js

--- js/index.js
+++ js/index.js
@@ -4,32 +4,32 @@ const mainContent = document.getElementsByTagName('main')[0]
 const htmlDocument = document.getElementsByTagName('html')[0]
 
 const isMenuVisible = () => {
-return document.querySelector(".navbar-burger").computedStyleMap().get("display") != 'none'
+    return window.getComputedStyle(document.querySelector(".navbar-burger")).display != 'none'
 }
 
 const isNavBurgerActive = () => {
-return document.querySelector(".navbar-burger").classList.contains("is-active")
+    return document.querySelector(".navbar-burger").classList.contains("is-active")
 }
 
 const animateNavMenu = () => {
-document.getElementById("navMenu").classList.toggle("animate__slideInRight") 
+    document.getElementById("navMenu").classList.toggle("animate__slideInRight") 
 }
 
 const toggleMenu = () => {
-animateNavMenu();
-navbarItems.forEach(element => {
-    element.classList.toggle('is-active');
-});
+    animateNavMenu();
+    navbarItems.forEach(element => {
+        element.classList.toggle('is-active');
+    });
 
-mainContent.classList.toggle('blured');
+    mainContent.classList.toggle('blured');
 
-htmlDocument.classList.toggle('hide-scrollbar')
+    htmlDocument.classList.toggle('hide-scrollbar')
 
-if (mainContent.classList.contains('blured')) {
-    disableScroll();
-} else {
-    enableScroll();
-}
+    if (mainContent.classList.contains('blured')) {
+        disableScroll();
+    } else {
+        enableScroll();
+    }
 }
 
 const navigationKeys = {37: 1, 38: 1, 39: 1, 40: 1};
@@ -44,9 +44,9 @@ if (navigationKeys[e.keyCode]) {
 }
 var supportsPassive = false;
 try {
-window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-}));
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; } 
+    }));
 } catch(e) {}
 
 const wheelOpt = supportsPassive ? { passive: false } : false;
@@ -54,34 +54,34 @@ const wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mouse
 
 // call this to Disable
 function disableScroll() {
-window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-window.addEventListener('keydown', preventDefaultScrollKeys, false);
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultScrollKeys, false);
 }
 
 // call this to Enable
 function enableScroll() {
-window.removeEventListener('DOMMouseScroll', preventDefault, false);
-window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-window.removeEventListener('touchmove', preventDefault, wheelOpt);
-window.removeEventListener('keydown', preventDefaultScrollKeys, false);
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultScrollKeys, false);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-const navbarBurger = document.querySelector('.navbar-burger')
-navbarItems.push(navbarBurger)
-navbarItems.push(document.getElementById(navbarBurger.dataset.target))
-navbarBurger.addEventListener('click', () => {
-    toggleMenu();
-});
-
-const navbarStartItems = document.querySelectorAll('.navbar-start .navbar-item')
-navbarStartItems.forEach(el => {
-    el.addEventListener('click', () => {
-    if (isMenuVisible()) {
+    const navbarBurger = document.querySelector('.navbar-burger')
+    navbarItems.push(navbarBurger)
+    navbarItems.push(document.getElementById(navbarBurger.dataset.target))
+    navbarBurger.addEventListener('click', () => {
         toggleMenu();
-    }
+    });
+
+    const navbarStartItems = document.querySelectorAll('.navbar-start .navbar-item')
+    navbarStartItems.forEach(el => {
+        el.addEventListener('click', () => {
+            if (isMenuVisible()) {
+                toggleMenu();
+            }
+        })
     })
-})
 })
